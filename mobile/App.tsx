50a6393c2862623d0de511a671c1df0a45adabcf
gabrielrguarini <gabrielrguarini@gmail.com>
2022-09-22 19:51:25 -0300

    finish screen

---
+++ mobile/App.tsx
@@ -0,0 +1,32 @@
+import { StatusBar } from 'react-native';
+
+import { 
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+} from '@expo-google-fonts/inter'
+import {Home} from './src/screens/Home'
+import { Background } from './src/components/Background';
+import { Loding } from './src/components/Loding';
+
+export default function App() {
+
+  const [fontsLoaded]  = useFonts({  
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  });
+
+  return (
+    <Background>
+      <StatusBar barStyle='light-content'
+      backgroundColor='transparent'
+      translucent
+      />
+      {fontsLoaded ? <Home/> : <Loding/>} 
+    </Background>
+  );
+}
